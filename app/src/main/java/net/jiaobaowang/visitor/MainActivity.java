72c240a5eec350dae474087f6decde1a9f875bc7
BugHunter1234 <444811716@qq.com>
2017-12-26 11:59:32 +0800

    check tianbo sdk

--- app/src/main/java/net/jiaobaowang/visitor/MainActivity.java
+++ app/src/main/java/net/jiaobaowang/visitor/MainActivity.java
@@ -1,13 +1,22 @@
 package net.jiaobaowang.visitor;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.telpo.tps550.api.util.StringUtil;
+import com.telpo.tps550.api.util.SystemUtil;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int deviceType = SystemUtil.getDeviceType();
+        StringUtil.DeviceModelEnum[] values = StringUtil.DeviceModelEnum.values();
+        String deviceName = "设备型号:" + values[deviceType];
+        Log.i(TAG, deviceName);
     }
 }
