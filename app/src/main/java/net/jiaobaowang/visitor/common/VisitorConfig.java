5d98eade8f22240f33da3971183a18cc3b9324e8
BugHunter1234 <444811716@qq.com>
2018-01-17 15:18:39 +0800

    增加七牛权限配置

--- app/src/main/java/net/jiaobaowang/visitor/common/VisitorConfig.java
+++ app/src/main/java/net/jiaobaowang/visitor/common/VisitorConfig.java
@@ -25,6 +25,31 @@ public class VisitorConfig {
      * 获取访客列表
      */
     public static final String VISITOR_API_LIST = VISITOR_MAIN_URL + "/api/list";
+    /**
+     * 七牛获取上传token
+     */
+    public static final String QINIU_GET_UPLOAD_TOKEN = "https://jbyc.jiaobaowang.net:8445/Api/QiNiu/GetUpLoadToKen";
+    /**
+     * 七牛获取下载token
+     */
+    public static final String QINIU_GET_DOWNLOAD_TOKEN = "https://jbyc.jiaobaowang.net:8445/Api/QiNiu/GetAccess";
+    /**
+     * 七牛批量删除文件
+     */
+    public static final String QINIU_DELETE_FILE = "https://jbyc.jiaobaowang.net:8445/Api/QiNiu/Delete";
+    /**
+     * 访客系统 七牛项目id
+     */
+    public static final String QINIU_VISITOR_SYSTEM_APP_ID = "10";
+    /**
+     * 访客系统 七牛密钥
+     */
+    public static final String QINIU_VISITOR_SYSTEM_SECRET_KEY = "jsy0117@";
+    /**
+     * 访客系统 七牛第一前缀名
+     */
+    public static final String QINIU_VISITOR_SYSTEM_FILE_FIRST_NAME = "fangkesys/";
+
     public static final String VISIT_LOCAL_STORAGE = "net.jiaobaowang.visitor.localStorage";//存储到本地的key
     public static final String VISIT_LOCAL_SCHOOL_ID = "net.jiaobangwang.visitor.school_id";//学校id
     public static final String VISIT_LOCAL_TOKEN = "net.jiaobaowang.visitor.token";//token
--- app/src/main/java/net/jiaobaowang/visitor/printer/VisitorFormDetailsActivity.java
+++ app/src/main/java/net/jiaobaowang/visitor/printer/VisitorFormDetailsActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import net.jiaobaowang.visitor.R;
+import net.jiaobaowang.visitor.common.VisitorConstant;
+import net.jiaobaowang.visitor.entity.VisitRecord;
 
 public class VisitorFormDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        VisitRecord visitRecord = (VisitRecord) getIntent().getSerializableExtra(VisitorConstant.INTENT_PUT_EXTRA_DATA);
         setContentView(R.layout.activity_visitor_form_details);
     }
 }
