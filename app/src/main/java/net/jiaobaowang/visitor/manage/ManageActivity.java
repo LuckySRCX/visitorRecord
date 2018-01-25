0239d024c93a3e37ff2b8eab280812a09f162358
BugHunter1234 <444811716@qq.com>
2018-01-25 15:07:33 +0800

    调整冲突

--- app/src/main/java/net/jiaobaowang/visitor/manage/ManageActivity.java
+++ app/src/main/java/net/jiaobaowang/visitor/manage/ManageActivity.java
@@ -9,9 +9,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.util.Log;
 
 import com.google.zxing.other.BeepManager;
